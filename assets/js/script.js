81d5bc971375815fcf36e3d656c96d01f054dce6
Sam <samuelhalloum@gmail.com>
2023-04-15 21:19:54 -0500

    added code for past, present future styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var timeBlocks = $(".row")
+var currentHour = dayjs().format('H')
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -14,6 +18,16 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+    for (var block of timeBlocks){
+      if(Number(block.id) > currentHour){
+      block.classList.replace(block.classList[2], "future")
+    }else if(Number(block.id) < currentHour){
+      block.classList.replace(block.classList[2], "past")
+    }else{
+      block.classList.replace(block.classList[2], "present")
+  }
+}
+
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
